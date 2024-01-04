462a59f4f4ca9a7237cb00951dc5709b1f0713b3
hidayatambo <hidayatsyahidin5@gmail.com>
2024-01-04 17:34:55 +0700

    perbaiki grouping assets

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -9,8 +9,8 @@
     <meta name="keywords" content="landing template, Cuba landing template,responsive landing
         template, web app">
     <meta name="author" content="pixelstrap">
-    <link rel="icon" href="{{ url('') }}images/favicon.webp" type="image/x-icon">
-    <link rel="shortcut icon" href="{{ url('') }}images/favicon.webp" type="image/x-icon">
+    <link rel="icon" href="{{ asset('landing-page/images/favicon.webp') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('landing-page/images/favicon.webp') }}" type="image/x-icon">
     <title>{{ $title ?? 'Page Title' }}</title>
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap"
@@ -21,21 +21,21 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
     <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&amp;display=swap" rel="stylesheet">
     <!-- Swiper css -->
-    <link rel="stylesheet" type="text/css" href="{{ url('css/vendors/swiper/swiper.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ url('css/vendors/swiper/swiper-bundle.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('landing-page/css/vendors/swiper/swiper.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('landing-page/css/vendors/swiper/swiper-bundle.min.css') }}">
 
     <!-- Font awesome css -->
-    <link rel="stylesheet" type="text/css" href="{{ url('css/vendors/fontawesome/all.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('landing-page/css/vendors/fontawesome/all.css') }}">
 
     <!-- Animate css -->
-    <link rel="stylesheet" type="text/css" href="{{ url('css/vendors/animate.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('landing-page/css/vendors/animate.css') }}">
 
     <!-- App css-->
-    <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('landing-page/css/style.css') }}">
 </head>
 
 <body class="layout-3">
-    <div class="loader-wrapper">
+    {{-- <div class="loader-wrapper">
         <div class="loader-index"> <span></span></div><svg>
             <defs></defs>
             <filter id="goo">
@@ -44,22 +44,22 @@
                 </fecolormatrix>
             </filter>
         </svg>
-    </div>
+    </div> --}}
     <div class="tap-top"><i class="fa-solid fa-angles-up"></i></div>
     {{ $slot }}
 
     <!-- bootstrap js -->
-    <script src="{{ url('js/vendors/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('landing-page/js/vendors/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
 
     <!-- swiper js -->
-    <script src="{{ url('js/vendors/swiper/swiper-bundle.min.js') }}"></script>
-    <script src="{{ url('js/custom-swiper.js') }}"></script>
+    <script src="{{ asset('landing-page/js/vendors/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('landing-page/js/custom-swiper.js') }}"></script>
 
     <!-- font awesome js -->
-    <script src="{{ url('js/vendors/fontawesome/all.min.js') }}"></script>
+    <script src="{{ asset('landing-page/js/vendors/fontawesome/all.min.js') }}"></script>
 
 
-    <script src="{{ url('js/script.js') }}"></script>
+    <script src="{{ asset('landing-page/js/script.js') }}"></script>
 
 </body>
 
